6849dc1e26ffe648ae2026e870baf9cec6bb8cf0
LovelyFoshou <rocky.dr@foxmail.com>
2024-04-05 18:59:30 +0800

    Fix Bug: Server can't login - Init Error

--- src/main/java/me/foshou/plugins/anarchy/core/Main.java
+++ src/main/java/me/foshou/plugins/anarchy/core/Main.java
@@ -1,16 +1,28 @@
 package me.foshou.plugins.anarchy.core;
 
+import me.foshou.plugins.anarchy.core.features.Register;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
-    public static final FileConfiguration config = Main.getProvidingPlugin(Main.class).getConfig();
-    public static final Main plugin = (Main) Main.getProvidingPlugin(Main.class);
+    public static FileConfiguration config;
+    public static Main plugin;
+
+    @Override
+    public void onLoad() {
+        plugin = this;
+        config = getConfig();
+    }
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
+        try {
+            new Register().load();
+        } catch (Exception ignored) {
+            System.err.println(ignored.getMessage());
+        }
     }
 
     @Override
